b386ad8205a95a0cbc918480fe156bbc82932e91
kongarasu <kongarasu.l@gmail.com>
2024-07-25 12:53:05 +0530

    updated web_server

--- web_server.js
+++ web_server.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 3600; // port
+const PORT = process.env.PORT || 3500; // port
 
-// '/' refers root file
-app.get('^/$ || /index(.html)?',(req,res) =>{
-    res.sendFile(path.join(__dirname,'views','index.html')); // sending index.html as home.
-}) 
-/** The corrected regular expression ^/$|| /index(.html)? ensures that the route handler matches requests to the root URL, /index, or /index.html. This is useful for cases where you want to serve the same HTML file for multiple similar paths.*/
-app.get('/new-page(.html)?',(req,res) =>{
-    res.sendFile(path.join(__dirname,'views','new-page.html')); // sending new-page.html as '/new-page.html'.
-}) 
-app.get('/old-page(.html)?',(req,res)=>{
-    res.redirect(301,'new-page.html');// sending new-page insted of old-page with 301 code
-})
-app.get('/*',(req,res)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
-}) /**This route acts as a catch-all for any requests that don't match previously defined routes. It serves a 404.html file, typically containing a "404 Not Found" error message, to indicate that the requested resource could not be found on the server. */
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// Serve the root file
+app.get(/^\/$|\/index(.html)?/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html')); // sending index.html as home.
+});
 
-  
+// Serve the new-page.html
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html')); // sending new-page.html as '/new-page.html'.
+});
+
+// Redirect old-page.html to new-page.html with 301 status code
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); // sending new-page instead of old-page with 301 code
+});
+
+// Route for hello.html
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('try to load hello.html');
+    next();
+}, (req, res) => {
+    res.send("hi hello konlak");
+});
+
+// Serve 404.html for any other routes 
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // serving 404.html for any unmatched routes
+});
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
